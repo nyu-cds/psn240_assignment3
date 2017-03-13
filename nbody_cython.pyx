7e9feb83daaa5a58b947b6ee86a1cfdfc1addaf2
Sudhir Nallam <psn240@nyu.edu>
2017-03-12 23:45:16 -0400

    Assignement 7

---
+++ nbody_cython.pyx
@@ -0,0 +1,178 @@
+"""
+    N-body simulation.
+    Changed all the varaibles to cdef
+    Changed the function arguments to C type
+    Used numpy array with efficient indexing
+    Time reduced from 40sec to 20 sec
+    Realtive Speed R = 40/20 = 2
+"""
+import numpy as np
+from itertools import combinations
+'''
+Created numpy arrays with efficient indexing
+'''
+cdef advance(double dt, System* BODIES):
+    '''
+        advance the system one timestep
+    '''
+    #cdef double m, m1, m2, dx, dy, dz, mag, b_m1, b_m2
+    for (body1, body2) in list(combinations(range(5),2)):
+        [x1, y1, z1] = np.array(BODIES[body1].position, dtype=np.double)
+        v1 = np.array(BODIES[body1].velocity, dtype=np.double)
+        m1 = BODIES[body1].mass
+        [x2, y2, z2] = np.array(BODIES[body2].position, dtype=np.double)
+        v2 = np.array(BODIES[body2].velocity, dtype=np.double)
+        m2 = BODIES[body2].mass
+        #(dx, dy, dz) = compute_deltas(x1, x2, y1, y2, z1, z2)
+        (dx, dy, dz) = (x1-x2, y1-y2, z1-z2)
+        mag = dt * (pow((dx * dx + dy * dy + dz * dz), -1.5))
+        b_m1 = m1 * mag
+        b_m2 = m2 * mag
+        v1[0] -= dx * b_m2 
+        v1[1] -= dy * b_m2
+        v1[2] -= dz * b_m2
+        v2[0] += dx * b_m1
+        v2[1] += dy * b_m1
+        v2[2] += dz * b_m1
+
+
+    for body in range(sizeof(BODIES)):
+        r = np.array(BODIES[body].position, dtype=np.double)
+        [vx,vy,vz] = np.array(BODIES[body].velocity, dtype=np.double)
+        m = BODIES[body].mass
+        r[0] += dt * vx
+        r[1] += dt * vy
+        r[2] += dt * vz
+        #update_rs(r, dt, vx, vy, vz)
+
+
+    
+cdef report_energy(System* BODIES, double e=0.0):
+    '''
+        compute the energy and return it so that it can be printed
+    '''        
+    #cdef double m, m1, m2
+    for (body1, body2) in list(combinations(range(5),2)):
+        [x1, y1, z1] = np.array(BODIES[body1].position, dtype=np.double)
+        v1 = np.array(BODIES[body1].velocity, dtype=np.double)
+        m1 = BODIES[body1].mass
+        [x2, y2, z2] = np.array(BODIES[body2].position, dtype=np.double)
+        v2 = np.array(BODIES[body2].velocity, dtype=np.double)
+        m2 = BODIES[body2].mass
+        #(dx, dy, dz) = compute_deltas(x1, x2, y1, y2, z1, z2)
+        (dx, dy, dz) = (x1-x2, y1-y2, z1-z2)
+        #e -= compute_energy(m1, m2, dx, dy, dz)
+        e -= (m1 * m2) / ((dx * dx + dy * dy + dz * dz) ** 0.5)
+
+    for body in range(sizeof(BODIES)):
+        [vx,vy,vz] = np.array(BODIES[body].velocity, dtype=np.double)
+        m = BODIES[body].mass
+        e += m * (vx * vx + vy * vy + vz * vz) / 2.
+        
+    return e
+
+
+cdef offset_momentum(System ref, System* BODIES, double px=0.0,double py=0.0,double pz=0.0):
+    '''
+        ref is the body in the center of the system
+        offset values from this reference
+    '''
+    #cdef double m
+    for body in range(sizeof(BODIES)):
+        v = np.array(BODIES[body].velocity, dtype=np.double)
+        m = BODIES[body].mass
+        px -= v[0] * m
+        py -= v[1] * m
+        pz -= v[2] * m
+        
+    v = np.array(ref.velocity, dtype=np.double)
+    m = ref.mass
+    v[0] = px / m
+    v[1] = py / m
+    v[2] = pz / m
+    
+cdef double PI = 3.14159265358979323
+cdef double SOLAR_MASS = 4 * PI * PI
+cdef float DAYS_PER_YEAR = 365.24
+    
+cdef struct System:
+        double position[3]
+        double velocity[3]
+        double mass
+        
+'''
+Changed all the variables to cdef, Created anew Bodies object with struct.
+'''
+def nbody(int loops, str reference, int iterations):
+    '''
+        nbody simulation
+        loops - number of loops to run
+        reference - body at center of system
+        iterations - number of timesteps to advance
+    '''
+
+       
+    cdef double PI = 3.14159265358979323
+    cdef double SOLAR_MASS = 4 * PI * PI
+    cdef float DAYS_PER_YEAR = 365.24
+    
+    cdef System BODIES2[5] 
+    
+    BODIES2[0].position = [0.0, 0.0, 0.0]
+    BODIES2[0].velocity = [0.0, 0.0, 0.0]
+    BODIES2[0].mass = SOLAR_MASS
+    
+    BODIES2[1].position = [4.84143144246472090e+00,
+                     -1.16032004402742839e+00,
+                     -1.03622044471123109e-01]
+    BODIES2[1].velocity = [1.66007664274403694e-03 * DAYS_PER_YEAR,
+                     7.69901118419740425e-03 * DAYS_PER_YEAR,
+                     -6.90460016972063023e-05 * DAYS_PER_YEAR]
+    BODIES2[1].mass = 9.54791938424326609e-04 * SOLAR_MASS
+    
+    BODIES2[2].position = [8.34336671824457987e+00,
+                    4.12479856412430479e+00,
+                    -4.03523417114321381e-01]
+    BODIES2[2].velocity = [-2.76742510726862411e-03 * DAYS_PER_YEAR,
+                    4.99852801234917238e-03 * DAYS_PER_YEAR,
+                    2.30417297573763929e-05 * DAYS_PER_YEAR]
+    BODIES2[2].mass = 2.85885980666130812e-04 * SOLAR_MASS
+    
+    BODIES2[3].position = [1.28943695621391310e+01,
+                    -1.51111514016986312e+01,
+                    -2.23307578892655734e-01]
+    BODIES2[3].velocity = [2.96460137564761618e-03 * DAYS_PER_YEAR,
+                    2.37847173959480950e-03 * DAYS_PER_YEAR,
+                    -2.96589568540237556e-05 * DAYS_PER_YEAR]
+    BODIES2[3].mass = 4.36624404335156298e-05 * SOLAR_MASS
+    
+    BODIES2[4].position = [1.53796971148509165e+01,
+                     -2.59193146099879641e+01,
+                     1.79258772950371181e-01]
+    BODIES2[4].velocity = [2.68067772490389322e-03 * DAYS_PER_YEAR,
+                     1.62824170038242295e-03 * DAYS_PER_YEAR,
+                     -9.51592254519715870e-05 * DAYS_PER_YEAR]
+    BODIES2[4].mass = 5.15138902046611451e-05 * SOLAR_MASS
+    
+    
+    cdef System ref
+    if reference == 'sun':
+         ref = BODIES2[0]
+    elif reference == 'jupiter':
+         ref = BODIES2[1]   
+    elif reference == 'saturn':
+         ref = BODIES2[2]
+    elif reference == 'uranus':
+         ref = BODIES2[3]
+    else: 
+         ref = BODIES2[4]
+    
+    # Set up global state
+    offset_momentum(ref, BODIES2)
+
+    for _ in range(loops):
+        #report_energy()
+        for _ in range(iterations):
+            advance(0.01, BODIES2)
+        print(report_energy(BODIES2))
+
